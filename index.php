f841189e6b1e712a38325e602e4231e940ea3eca
esteban1234 <tva_libra@hotmail.com>
2018-04-20 17:20:30 -0500

    inicio de pagina

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>SERVICIOS Y REFACCIONES DEL MAYAB</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="SERVICIOS Y REFACCIONES DEL MAYAB">
+    <META NAME="Author" CONTENT="[http://seremaservicios.com/]"/>
+    <META name="Description" content=""/>
+    <META NAME="Keywords" CONTENT=""/>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    
+
+    <div class="banner"  id="INICIO">
+    <div class="fondo">
+      <div class="menu">
+        <nav class="pull-right">
+          <ul>
+            <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+            <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+            <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+            <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+          </ul>
+        </nav>
+
+        <figure class="logo pull-left">
+          <img src="img/serema.png" alt="SEREMA" class="img-responsive">
+        </figure>
+
+        <div class="openclose">
+          <i class="fa fa-bars open" aria-hidden="true"></i>
+        </div>  
+      </div>
+
+      <h1 class="tit-cc">Tus Autorrepuestos de Confianza</h1>
+      <p class="subtit-cc">Tenemos todas las partes de mayor venta</p>
+
+      <div class="boton">
+        <a data-scroll href="#CONTACTO" aria-pressed="true" class="btn btn-warning btn-outline btn-lg btn-contacto">CONTACTANOS</a>
+      </div>
+
+    </div>
+  </div>
+
+
+<section class="seccion1" id="SERVICIOS" >
+ 
+</section>
+
+<section class="seccion2" id="NOSOTROS">
+
+</section>
+
+
+<section class="seccion3" id="CONTACTO">
+
+</section>
+
+
+<footer>
+<p>Derechos de autor 2018 © |  SERVICIOS Y REFACCIONES DEL MAYAB SA DE CV</p>
+</footer>
+
+<a class="go-top" href="#">Ir Arriba</a>
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
